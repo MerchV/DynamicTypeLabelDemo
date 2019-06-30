79d85b4a0a19c76b60fed5a1d17a3b378459e209
Merch Visoiu <merch@merchv.com>
2019-06-29 23:06:15 -0400

    Initial Commit

---
+++ DynamicTypeLabelDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicTypeLabelDemo
+//
+//  Created by Merch on 2019-06-29.
+//  Copyright © 2019 Merch Visoiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
